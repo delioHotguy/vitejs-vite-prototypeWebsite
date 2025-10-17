f7b2d578351c2306933a81a68e5f52dac6df3c67
delioHotguy <neal_aracil@cca.edu>
2025-10-17 15:05:09 -0700

    commit again struggle

--- src/assets/App.test.jsx
+++ src/assets/App.test.jsx
@@ -3,7 +3,9 @@ import { test, expect, it } from 'vitest';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 
-import App from "./App"; 
+import App from ".assets/App"; 
+import App from "@/App";
+
 
 test('basic test', () => {
   expect(1 + 1).toBe(2);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   test: {
