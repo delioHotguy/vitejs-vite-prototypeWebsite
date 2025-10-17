545a48690431d57e7b5f84242b76821ce847e2d6
delioHotguy <neal_aracil@cca.edu>
2025-10-17 12:57:00 -0700

    test clean up

--- src/assets/App.test.js
+++ src/assets/App.test.js
@@ -1,5 +1,3 @@
-//<div id="info">Nealie Wheelie</div>
-// src/App.test.js
 test('basic test', () => {
   expect(1 + 1).toBe(2);
 });
@@ -12,33 +10,3 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOMClient.createRoot(div).render(<App />);
 });
-
-
-import * as THREE from 'three';
-// import THREE from "three.min.js"; (outdated/old/backup)
-
-//create camera for scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-//use Web Gl for 3D rendering
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-//bring in cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-//Animate in scene
-function animate() {
-     //create rotation animation
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
-  }
-  renderer.setAnimationLoop( animate );
