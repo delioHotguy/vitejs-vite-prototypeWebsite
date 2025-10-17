9a3f87551ab0177d34eef2cd471a945dcedf2a71
delioHotguy <neal_aracil@cca.edu>
2025-10-17 12:58:31 -0700

    reformat

--- src/assets/App.test.js
+++ src/assets/App.test.js
@@ -1,10 +1,10 @@
-test('basic test', () => {
-  expect(1 + 1).toBe(2);
-});
-
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import App from './App';
+
+test('basic test', () => {
+  expect(1 + 1).toBe(2);
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
