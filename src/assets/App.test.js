c79eb0c2deba8e45439f0018d2f913c5055c1458
delioHotguy <neal_aracil@cca.edu>
2025-10-17 09:47:41 -0700

    adding react

--- src/assets/App.test.js
+++ src/assets/App.test.js
@@ -4,6 +4,16 @@ test('basic test', () => {
   expect(1 + 1).toBe(2);
 });
 
+import React from 'react';
+import ReactDOMClient from 'react-dom/client';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOMClient.createRoot(div).render(<App />);
+});
+
+
 import * as THREE from 'three';
 // import THREE from "three.min.js"; (outdated/old/backup)
 
