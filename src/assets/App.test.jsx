0dd199e252594debba5e4003a822e963725fee5e
delioHotguy <neal_aracil@cca.edu>
2025-10-17 14:58:15 -0700

    jsx file change

--- src/assets/App.test.jsx
+++ src/assets/App.test.jsx
@@ -3,7 +3,7 @@ import { test, expect, it } from 'vitest';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 
-import App from "../../App"; 
+import App from "./App"; 
 
 test('basic test', () => {
   expect(1 + 1).toBe(2);
