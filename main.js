83e29478355b810d8ddeaf4196716ec056347c25
delioHotguy <neal_aracil@cca.edu>
2025-10-17 15:47:59 -0700

    committ

---
+++ main.js
@@ -0,0 +1,35 @@
+import './style.css';
+
+import React from 'react';
+import * as THREE from 'three';
+// import THREE from "three.min.js"; (outdated/old/backup)
+
+console.log("main.js loaded!");
+
+//create camera for scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+//use Web Gl for 3D rendering
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//bring in cube
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+//Animate in scene
+function animate() {
+     //create rotation animation
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+  }
+  renderer.setAnimationLoop( animate );
+
+  this.camera.position.set(0,0,4)
